c9faff56afed0c40108b40986520622d40aa6b63
Trina Nixon <TrinaNixon7@gmail.com>
2022-09-20 18:01:20 -0600

    express server, router object, and GET

---
+++ index.js
@@ -0,0 +1,11 @@
+//Bring in the express server and create application
+let express = require("express");
+let app = express();
+
+//use the express Router object
+let router = express.Router();
+
+//Create GET to return a list of all pies
+router.get("/", function (req, res, next) {
+  res.send("Apple");
+});
