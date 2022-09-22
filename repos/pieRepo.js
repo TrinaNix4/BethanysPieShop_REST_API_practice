d90dbecc437e8977be4c3d978ff1b78d4e9613b9
Trina Nixon <TrinaNixon7@gmail.com>
2022-09-22 12:52:12 -0600

    created pieRepo

---
+++ repos/pieRepo.js
@@ -0,0 +1,11 @@
+let pieRepo = {
+  //create a function called 'get' that returns a
+  //hard-coded array
+  get: function () {
+    return [
+      { id: 1, name: "Apple" },
+      { id: 2, name: "Cherry" },
+      { id: 3, name: "Peach" },
+    ];
+  },
+};
