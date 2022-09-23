385cb63c68a900424e5a3cb103cc7a7d9b7d68f6
Trina Nixon <TrinaNixon7@gmail.com>
2022-09-22 21:24:16 -0600

    changing hard coded array to reading from a file

--- repos/pieRepo.js
+++ repos/pieRepo.js
@@ -1,12 +1,21 @@
+//fs is a built in node module that knows how to work with reading and writing files
+let fs = require("fs");
+
+const FILE_NAME = "./assets/pies.json";
+
 let pieRepo = {
-  //create a function called 'get' that returns a
-  //hard-coded array
-  get: function () {
-    return [
-      { id: 1, name: "Apple" },
-      { id: 2, name: "Cherry" },
-      { id: 3, name: "Peach" },
-    ];
+  get: function (resolve, reject) {
+    //read file on that file_name, the functiosn brings in an error and the data itself
+    fs.readFile(FILE_NAME, function (err, data) {
+      //if in error, going to call the reject callback, passing the error object back
+      if (err) {
+        reject(err);
+        //if everything correct,going to call the resolve callback
+        //JSON.parse the data so grabbing the data and converting it to actual JSON
+      } else {
+        resolve(JSON.parse(data));
+      }
+    });
   },
 };
 
